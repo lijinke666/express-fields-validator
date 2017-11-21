509a5f30712b336787fafd7e94476194546524af
jinke <1359518268@qq.com>
2017-11-21 11:41:33 +0800

    feat:放弃了 :(

--- example/router/route1.js
+++ example/router/route1.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const { check, validationResult } = require('express-validator/check');
 
 
 router.get('/route1', (req, res, next) => {
@@ -8,13 +9,32 @@ router.get('/route1', (req, res, next) => {
     next()
 })
 
-router.post('/route2', (req, res, next) => {
+router.post('/route2',[
+    check('name')
+    .isEmail().withMessage('must be an email')
+    .trim()
+    .normalizeEmail()
+
+    .custom(value => {
+      return findUserByEmail(value).then(user => {
+        throw new Error('this email is already in use');
+      })
+    }),
+], (req, res, next) => {
     const {
         id,
         name,
         sex
     } = req.body
-
+    
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.mapped() });
+    }
+  
+    // matchedData returns only the subset of data validated by the middleware
+    const user = matchedData(req);
+    createUser(user).then(user => res.json(user));
     res.resRawData = { id, name, sex }
     next()
 })
